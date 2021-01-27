86a3c50ba417b7cbc65b28fc1028058920b497fa
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-27 15:31:46 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,12 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Mohanur" }, "geometry": { "type": "Point", "coordinates": [ 78.140187432519838, 11.06432162560805, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Velur" }, "geometry": { "type": "Point", "coordinates": [ 78.002613125287496, 11.11293677944332, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Rasipuram" }, "geometry": { "type": "Point", "coordinates": [ 78.189929481076362, 11.46007775743773, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Namakkal" }, "geometry": { "type": "Point", "coordinates": [ 78.165721774733655, 11.22285839078552, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tiruchengode" }, "geometry": { "type": "Point", "coordinates": [ 77.898233738575726, 11.38269762816174, -999999.0 ] } }
+]
+}
---
+++ data/VillageWiseFloodVulnerablitiy_5.js
@@ -0,0 +1,8 @@
+var json_VillageWiseFloodVulnerablitiy_5 = {
+"type": "FeatureCollection",
+"name": "VillageWiseFloodVulnerablitiy_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 78.123563807702169, 11.322277297981472 ] } }
+]
+}
